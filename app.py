43764efbe03be166b5c93c2ed1c9002f5a27e28f
marcuscolle <marcusvcolle@gmail.com>
2020-01-16 22:28:04 +0000

    DB returning

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'cook_book'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
-app.route('/')
-def hello():
-    return 'testing my route'
+mongo = PyMongo(app)    
 
 
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html", recipe_name=mongo.db.recipe_name.find())
 
-if __name__=='__main__':
-    app.run(host=os.environ.get('IP')
-            port=int(os.environ.get('PORT'))
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
-
-            
